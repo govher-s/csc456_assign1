06513368eaa949573d6d2f43fef13c044cf4e64b
Govher <sapardurdyyevagovher@gmail.com>
2025-10-19 16:35:30 -0500

    initial

---
+++ shell.cpp
@@ -0,0 +1,25 @@
+/*************************************
+ * ***********************************
+Name      : Govher Sapardurdyyeva    *
+Class     : CSC 456                  *
+Assignment: Programming Assignment 1 *
+Instructor: Dr. Juang                *
+Due Date  : November 2, 2025         *
+**************************************
+**************************************/
+#include <iostream>
+#include <string.h>
+#include <unistd.h>
+#include <cstdlib>
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!";
+    return 0;
+}
